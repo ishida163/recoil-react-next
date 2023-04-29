0219c94846565258f5cf6c922102aed31ecec382
ishida <tmp07i@aol.com>
2023-04-29 14:37:33 +0900

    onClick処理書き始め

--- src/pages/components/Question.tsx
+++ src/pages/components/Question.tsx
@@ -6,11 +6,19 @@ interface QuestionProps {
   type: string;
 }
 
+const handleClick = (id:number,yesno:boolean) => {
+  // 【TODO】親コンポーネント化にデータを送る　RecoilかReduxかuseContext
+  return
+}
+
 const Question: React.FC<QuestionProps> = ({ id, question, type }) => (
   <div>
     <h3>{`Question ${id}`}</h3>
     <p>{`Type: ${type}`}</p>
     <p>{question}</p>
+    <button onClick={() => handleClick(id,true)}>YES</button>
+    <button onClick={() => handleClick(id,false)}>NO</button>
+    <hr/>
   </div>
 );
 
