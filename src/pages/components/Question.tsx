82370acf9d280ba9b2e8f6fe3e5ed4c71b0ff51b
ishida <tmp07i@aol.com>
2023-07-18 00:38:15 +0900

    YESの重複計上防止

--- src/pages/components/Question.tsx
+++ src/pages/components/Question.tsx
@@ -1,34 +1,37 @@
-import React from 'react';
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { storeAnswer } from "../states/answer";
-import { Answer } from '../../types/answer';
+  import React from 'react';
+  import { useRecoilCallback, useRecoilValue, useSetRecoilState } from "recoil";
+  import { storeAnswer } from "../states/answer";
+  import { Answer } from '../../types/answer';
 
-interface QuestionProps {
-  id: number;
-  question: string;
-  type: string;
-}
+  interface QuestionProps {
+    id: number;
+    question: string;
+    type: string;
+  }
 
-const Question = (props:QuestionProps) => {
-  const useAnswer = useRecoilValue(storeAnswer);
-  const setAnswer = useSetRecoilState(storeAnswer);
+  const Question = (props:QuestionProps) => {
+    const useAnswer = useRecoilValue(storeAnswer);
+    const setAnswer = useSetRecoilState(storeAnswer);
+    let answeredArray: number[] = [];
 
-const handleClick = (id:number,yesno:boolean,type:string) => {
+  const handleClick = (id:number,yesno:boolean,type:string) => {
+    //【TODO】noボタン押した時にfalseにする処理作る
+    if (!useAnswer[id-1]){
+        setAnswer([...useAnswer,{id,yesno,type}])
+    }
+    return
+  }
 
-  setAnswer([...useAnswer,{id,yesno,type}])
-  return
-}
+    return (
+    <div>
+      <h3>{`Question ${props.id}`}</h3>
+      <p>{`Type: ${props.type}`}</p>
+      <p>{props.question}</p>
+      <button onClick={() => handleClick(props.id,true,props.type)}>YES</button>
+      <button>NO</button>
+      <hr/>
+    </div>
+    )
+  }
 
-  return (
-  <div>
-    <h3>{`Question ${props.id}`}</h3>
-    <p>{`Type: ${props.type}`}</p>
-    <p>{props.question}</p>
-    <button onClick={() => handleClick(props.id,true,props.type)}>YES</button>
-    <button>NO</button>
-    <hr/>
-  </div>
-  )
-}
-
-export default Question;
+  export default Question;
